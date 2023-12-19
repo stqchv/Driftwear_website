10c5e9dcd46b6ec9b2e2211aee2cc5f39066d5dc
stqchv <148893477+stqchv@users.noreply.github.com>
2023-12-19 22:11:37 +0100

    idk

---
+++ src/newsletter.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Driftwear</title>
+
+    <link rel="icon" href="../images/browser_icon.png">
+
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/header.css">
+    <link rel="stylesheet" href="../styles/newsletter.css">
+    <link rel="stylesheet" href="../styles/footer.css">
+
+    <script src="https://kit.fontawesome.com/2bd77566a6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css">
+</head>
+<body>
+    
+
+    <!-- ---------------- UPHEADER ---------------- -->
+    <div class="upheader">
+        <a href="../src/newsletter.php"><p>GET EXTRA <strong>15%</strong> DISCOUNT</p></a>
+    </div>
+    <!-- ---------------- HEADER ---------------- -->
+    <header>
+        <a href="index.php" class="logo">
+            <img src="../images/logo.png" alt="Logo">
+        </a>
+
+        <div class="nav__links">
+            <a href="../src/shop.php">Shop</a>
+            <a href="../src/contact.php">Contact</a>
+        </div>
+
+        <div class="profile">
+            <?php
+                session_start();
+                if (isset($_SESSION["user"]) && $_SESSION["user"] == "yes") {
+                    echo "<a href='../src/logout.php' class='user'>Logout</a>";
+                    echo "<a href='../src/cart.php' class='cart'><i class='ri-shopping-bag-fill'></i></a>";
+                }
+                else {
+                    echo "<a href='../src/login.php' class='user'>Login</a>";
+                }
+            ?>
+        </div>
+    </header>
+
+    <div class="underbar"></div> <!-- place under navbar -->
+
+    <!-- ---------------- NEWSLETTER ---------------- -->
+    
+    <form id="newsletterForm" action="" method="post">
+        <label for="email">Twój adres e-mail:</label>
+        <input type="email" id="email" name="email">
+        <button type="submit">Zapisz się</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $kodRabatowy = "POLSL<3";
+        mail($email, "Twój kod rabatowy", "Dziękujemy za zapisanie się do newslettera. Twój kod rabatowy to: $kodRabatowy");
+    }
+    ?>
+
+    </div>
+    <!-- ---------------- FOOTER ---------------- -->
+
+    <footer>
+        <div class="footer_content">
+            <h3>Driftwear&trade;</h3>
+            <div class="content">
+                <a href="#">Shop</a>
+                <a href="#">Contact</a>
+                <a href="#">Website Terms</a>
+                <a href="#">Privacy Policy</a>
+            </div>
+            <div class="footer_bottom">
+                <p>&copy; 2023 Made by <span>Tomasz Stasiński & Jakub Pytlarz</span></p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="../scripts/script.js" async defender></script>
+    <script src="../scripts/cart.js" async defender></script>
+</body>
+</html>
